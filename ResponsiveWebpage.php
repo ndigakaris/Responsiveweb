1de7ecb8c9b769ab37fa6faf293c2cd1cb2d9914
ndigakaris <peterndiga1@gmail.com>
2023-07-25 07:42:01 +0300

    Update ResponsiveWebpage.php

--- ResponsiveWebpage.php
+++ ResponsiveWebpage.php
@@ -9,7 +9,7 @@
 	<div class="main">
 		<div class ="navbar">
 			<div class ="icon">
-				<h2 class = "logo"><a href="contactpage.html">Talk_To_Us</a></h2>
+				<h2 class = "logo"><a href="index.html">Talk_To_me</a></h2>
 			</div>
 	
 			<div class="menu">
